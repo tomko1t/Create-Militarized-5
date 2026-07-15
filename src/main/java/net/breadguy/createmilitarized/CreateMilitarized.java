2a2d27bf78473413d45f31e7941d5405314eea24
tomko1t <kubinatomas55@gmail.com>
2026-07-15 21:51:26 +0200

    added creative tab

--- src/main/java/net/breadguy/createmilitarized/CreateMilitarized.java
+++ src/main/java/net/breadguy/createmilitarized/CreateMilitarized.java
@@ -1,5 +1,6 @@
 package net.breadguy.createmilitarized;
 
+import net.breadguy.createmilitarized.item.ModCreativeModeTabs;
 import net.breadguy.createmilitarized.item.ModItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -34,6 +35,8 @@ public class CreateMilitarized {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
 
---
+++ src/main/java/net/breadguy/createmilitarized/item/ModCreativeModeTabs.java
@@ -0,0 +1,32 @@
+package net.breadguy.createmilitarized.item;
+
+import net.breadguy.createmilitarized.CreateMilitarized;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CreateMilitarized.MODID);
+
+    public static final Supplier<CreativeModeTab> CREATE_MILITARIZED_TAB = CREATIVE_MODE_TAB.register("create_militarized_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.STEEL_INGOT.get()))
+                    .title(Component.translatable("creativetab.createmilitarized.create_militarized_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.STEEL_INGOT);
+                        output.accept(ModItems.STEEL_SHEET);
+                        output.accept(ModItems.STEEL_MIXTURE);
+                        output.accept(ModItems.COPPER_WIRE);
+                        output.accept(ModItems.CAPACITOR);
+                        output.accept(ModItems.SULFUR);
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
